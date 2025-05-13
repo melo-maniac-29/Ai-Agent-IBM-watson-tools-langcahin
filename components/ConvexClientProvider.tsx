02c34b285b32b59610bfd41aaec8e43b82a3d471
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-14 02:14:48 +0530

    middle ware and convex client provider done

---
+++ components/ConvexClientProvider.tsx
@@ -0,0 +1,20 @@
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { use } from "react";
+
+const convex =new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function ConvexClientProvider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+   <ClerkProvider>
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+    </ConvexProviderWithClerk>
+   </ClerkProvider>
+  );
+}
