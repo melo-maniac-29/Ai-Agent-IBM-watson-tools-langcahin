218234acbe753e62c76526c2a37b0aa1be3e6008
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-14 13:28:24 +0530

    correct convex client provider and tailwind

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
-import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { use } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ReactNode } from "react";
 
-const convex =new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export default function ConvexClientProvider({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
-   <ClerkProvider>
-    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
-    </ConvexProviderWithClerk>
-   </ClerkProvider>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   );
 }
